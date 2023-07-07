b93b7dba7f5b75f9ecc6c633eab0f16433ed016b
Percy Zhang <10626042@qq.com>
2023-07-07 22:46:16 +0800

    第一次提交

---
+++ src/permission.js
@@ -0,0 +1,80 @@
+/*
+  permission文件的作用：
+  权限 -> 登陆拦截
+
+  如果有token，
+    如果访问login页面，直接去首页
+    如果去的首页 -> 想去就去
+
+  如果没有token
+    判断是否为白名单的路径
+    是 -> 直接去
+    不是 -> 拦截，重定向到登录页
+
+  白名单一般放一些没有权限的路径，比如 '/login' , '/404' 。。。等
+*/
+
+// import router from './router'
+// import store from './store'
+// import { Message } from 'element-ui'
+// import NProgress from 'nprogress' // progress bar
+// import 'nprogress/nprogress.css' // progress bar style
+// import { getToken } from '@/utils/auth' // get token from cookie
+// import getPageTitle from '@/utils/get-page-title'
+
+// NProgress.configure({ showSpinner: false }) // NProgress Configuration
+
+// const whiteList = ['/login'] // no redirect whitelist
+
+// router.beforeEach(async(to, from, next) => {
+//   // start progress bar
+//   NProgress.start()
+
+//   // set page title
+//   document.title = getPageTitle(to.meta.title)
+
+//   // determine whether the user has logged in
+//   const hasToken = getToken()
+
+//   if (hasToken) {
+//     if (to.path === '/login') {
+//       // if is logged in, redirect to the home page
+//       next({ path: '/' })
+//       NProgress.done()
+//     } else {
+//       const hasGetUserInfo = store.getters.name
+//       if (hasGetUserInfo) {
+//         next()
+//       } else {
+//         try {
+//           // get user info
+//           await store.dispatch('user/getInfo')
+
+//           next()
+//         } catch (error) {
+//           // remove token and go to login page to re-login
+//           await store.dispatch('user/resetToken')
+//           Message.error(error || 'Has Error')
+//           next(`/login?redirect=${to.path}`)
+//           NProgress.done()
+//         }
+//       }
+//     }
+//   } else {
+//     /* has no token*/
+
+//     if (whiteList.indexOf(to.path) !== -1) {
+//       // in the free login whitelist, go directly
+//       next()
+//     } else {
+//       // other pages that do not have permission to access are redirected to the login page.
+//       next(`/login?redirect=${to.path}`)
+//       NProgress.done()
+//     }
+//   }
+// })
+
+// router.afterEach(() => {
+//   // finish progress bar
+//   NProgress.done()
+// })
---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 网页标签栏的标题
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  /* navbar滚动时固定 */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  /* sidebar左边顶部logo */
+  sidebarLogo: true
+}
